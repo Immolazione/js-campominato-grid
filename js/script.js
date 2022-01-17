59cdca1b55d8bc2f62bd6fbb900b757f9b5d0f6b
Immolazione <federico.deered@gmail.com>
2022-01-17 20:42:45 +0100

    tried to add options

--- js/script.js
+++ js/script.js
@@ -10,12 +10,7 @@ console.log('JS OK');
 // ! RECUPERO GLI ELEMENTI DAL DOM
 const selectDifficulty = document.getElementById('number-of-cells');
 const grid = document.getElementById('grid');
-// const grid100 = document.getElementById('100');
-// const grid81 = document.getElementById('81');
-// const grid49 = document.getElementById('49');
-const diff1 = [];
-const diff2 = [];
-const diff3 = [];
+let diff = document.querySelectorAll('diff');
 const generateButton = document.getElementById('generate');
 
 // ! FUNZIONI NEUTRE
@@ -24,13 +19,26 @@ const generateButton = document.getElementById('generate');
 
 
 
-// CREO CELLE
-let cell100;
-let number100;
-for (i = 0; i <100; i++) {
-    cell100 = document.createElement('div');
-    cell100.classList.add('cell-100')
-    grid.appendChild(cell100);
-    number100 = (i + 1);
-    cell100.innerHTML = `${number100}`
-}
+console.log(diff.value);
+
+generateButton.addEventListener('click', function () {
+    if (diff.value === 100) {
+        diff = 100;
+    } else if (diff.value === 81) {
+        diff = 81;
+    } else if (diff.value === 49) {
+        diff = 49;
+    }
+    
+    // CREO CELLE
+    let cell100;
+    let number100;
+    for (i = 0; i < diff.value; i++) {
+        cell100 = document.createElement('div');
+        cell100.classList.add('cell-100')
+        grid.appendChild(cell100);
+        number100 = (i + 1);
+        cell100.innerHTML = `${number100}`;
+        console.log(number100);
+    }
+})
