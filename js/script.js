a5460dc4351f7ea46ece0bae6ef76021670cbebd
Immolazione <federico.deered@gmail.com>
2022-01-17 18:00:43 +0100

    create cell and print numbers

--- js/script.js
+++ js/script.js
@@ -7,14 +7,30 @@ console.log('JS OK');
 // con difficoltà 3 => tra 1 e 49
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
-// RECUPERO GLI ELEMENTI DAL DOM
+// ! RECUPERO GLI ELEMENTI DAL DOM
 const selectDifficulty = document.getElementById('number-of-cells');
 const grid = document.getElementById('grid');
-const grid100 = document.getElementById('100');
-const grid81 = document.getElementById('81');
-const grid49 = document.getElementById('49');
+// const grid100 = document.getElementById('100');
+// const grid81 = document.getElementById('81');
+// const grid49 = document.getElementById('49');
+const diff1 = [];
+const diff2 = [];
+const diff3 = [];
 const generateButton = document.getElementById('generate');
 
-// FUNZIONI NEUTRE
-const getRandNumber = (min, max) => Math.floor(Math.random() * (max - min) + 1 ) + min;
-console.log(getRandNumber(1, 100));
+// ! FUNZIONI NEUTRE
+// const getRandNumber = (min, max) => Math.floor(Math.random() * (max - min) + 1 ) + min;
+// console.log(getRandNumber(1, 100));
+
+
+
+// CREO CELLE
+let cell100;
+let number100;
+for (i = 0; i <100; i++) {
+    cell100 = document.createElement('div');
+    cell100.classList.add('cell-100')
+    grid.appendChild(cell100);
+    number100 = (i + 1);
+    cell100.innerHTML = `${number100}`
+}
