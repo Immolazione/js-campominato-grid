be73bdc78209b36465371878b601a8491ddbe793
Immolazione <federico.deered@gmail.com>
2022-01-18 13:54:10 +0100

    try to restart

--- js/script.js
+++ js/script.js
@@ -8,37 +8,29 @@ console.log('JS OK');
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
 // ! RECUPERO GLI ELEMENTI DAL DOM
-const selectDifficulty = document.getElementById('number-of-cells');
+const difficulty = document.getElementById('number-of-cells').value;
 const grid = document.getElementById('grid');
 const generateButton = document.getElementById('generate');
-console.log(selectDifficulty);
-// ! FUNZIONI NEUTRE
-// const getRandNumber = (min, max) => Math.floor(Math.random() * (max - min) + 1 ) + min;
-// console.log(getRandNumber(1, 100));
+console.log(difficulty);
 
-// AGGIUNGO OPTIONS
-
-
-
-
-
-
-let diff = document.querySelectorAll('option');
-console.log(diff);
 
 
 // GENERATE BUTTON
 generateButton.addEventListener('click', function () {
+    
 
     // CREO CELLE
-    let cell100;
-    let number100;
-    for (i = 0; i < diff.value; i++) {
-        cell100 = document.createElement('div');
-        cell100.classList.add('cell-100')
-        grid.appendChild(cell100);
-        number100 = (i + 1);
-        cell100.innerHTML = `${number100}`;
-        console.log(number100);
-    }
+
+
+
+    // let cell100;
+    // let number100;
+    // for (i = 0; i < difficulty; i++) {
+    //     cell100 = document.createElement('div');
+    //     cell100.classList.add('cell-100')
+    //     grid.appendChild(cell100);
+    //     number100 = (i + 1);
+    //     cell100.innerHTML = `${number100}`;
+    //     console.log(number100);
+    // }
 })
